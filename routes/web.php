f67ad8674f77bf52593cc86e3a203fdedf0cd3b3
salad-man1 <salahaldinaljoujah@gmail.com>
2024-08-22 15:44:54 +0300

    fixed the bug it was breeze overwriting the web.php routes

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDecisionController;
+use App\Http\Controllers\Admin\AdminServiceController;
+use App\Http\Controllers\User\UserDecisionController;
+use App\Http\Controllers\User\UserServiceController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-
+//breeze added
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -17,4 +22,16 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::resource('admin/services', AdminServiceController::class);
+
+Route::resource('admin/decisions', AdminDecisionController::class);
+
+Route::resource('/decisions',UserDecisionController::class);
+
+Route::resource('/services',UserServiceController::class);
+
+
+
 require __DIR__.'/auth.php';
+
