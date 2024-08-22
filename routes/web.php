a9eb375ac91af2fd828c6818ef7b74cc6ea234ba
salad-man1 <salahaldinaljoujah@gmail.com>
2024-08-22 15:37:09 +0300

    added breeze with a bug

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,20 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminDecisionController;
-use App\Http\Controllers\Admin\AdminServiceController;
-use App\Http\Controllers\User\UserDecisionController;
-use App\Http\Controllers\User\UserServiceController;
-use App\Http\Controllers\primarycontroller;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get(
-    '/',
-    [primarycontroller::class, "index"]
-);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('admin/services', AdminServiceController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('admin/decisions', AdminDecisionController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('/decisions',UserDecisionController::class);
-
-Route::resource('/services', UserServiceController::class);
-
+require __DIR__.'/auth.php';
