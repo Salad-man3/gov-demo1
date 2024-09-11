0d2d3ff8f4e40f867b278bc374a9f007935c1e3b
salad-man1 <salahaldinaljoujah@gmail.com>
2024-09-11 19:44:47 +0300

    minor bug fix

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -28,7 +28,7 @@ class LoginController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('admin.decisions.index', absolute: false));
+        return redirect()->intended(route('admin.news.index', absolute: false));
     }
 
     /**
